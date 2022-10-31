40d5f24e629972f0f51cebe7235cdab9c924182d
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-10-31 08:31:00 +0300

    init: start lora configure

---
+++ src/lora/lora.c
@@ -0,0 +1,6 @@
+/*
+ * lora.c
+ *
+ * Created: 31.10.2022 7:31:14
+ *  Author: Vadim
+ */ 
---
+++ src/lora/lora.h
@@ -0,0 +1,20 @@
+/*
+ * lora.h
+ *
+ * Created: 31.10.2022 7:31:05
+ *  Author: Vadim
+ */ 
+
+
+#ifndef LORA_H_
+#define LORA_H_
+
+//Инициализация LoRa
+uint8_t lora_init();
+
+//Код версии чипсета
+uint8_t lora_check_version();
+
+
+
+#endif /* LORA_H_ */
