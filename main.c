31cb8c729bbf03fe9fa25e68d8c623d7c35df1f8
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-01-24 11:01:41 +0300

    feat: complete transmitter

--- main.c
+++ main.c
@@ -8,17 +8,15 @@
 #define F_CPU 16000000UL
 #define BAUD 9600
 #define UBRR F_CPU/16/BAUD-1
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
 #include "lora.h"
-
 int main(void)
 {
     uint8_t btn_ticks_counter = 0; // Счетчик тиков удержания кнопки
+    uint8_t btn_click_counter = 0;
 	uart_init(UBRR);
-
     Speed sped = {
         {
             0b010,
@@ -26,7 +24,6 @@ int main(void)
             0b00,
         }
     };
-
     Option option = {
         {
             0b00,
@@ -36,8 +33,6 @@ int main(void)
             0b0
         }
     };
-
-    //Default config
     Config cnfg = {
         SAVE_CNFG,                //0xC0 - сохраняем настройки при отключении питания
         0x21,
@@ -46,14 +41,11 @@ int main(void)
         0x11,                    //channel (410 + (value * 1M)) = 433MHz
         option    //0x01000100 0x44
     };
-
     DDRE |= (1 << PE2);
     DDRE |= (1 << PE3);
-
     DDRE &= ~(1 << PE4); //Настройка вывода PE4 на вход для считывания показаний кнопки
     PORTE |= (1 << PE4);
     PORTE &= ~(1 << PE3);
-
     if (lora_init_with_config(cnfg) == 1) {
         lora_switch_mode(MODE_WAKE_UP);
         while (1) {
@@ -64,13 +56,20 @@ int main(void)
                 {
                     uart_transmit('H');
                 }
-                // Увеличиваем счетчик тиков, пока он не станет равен 10
                 if (btn_ticks_counter < 10)
                 {
-                    btn_ticks_counter++; // Увеличиваем на 1 счетчик тиков
+                    btn_ticks_counter++;
                 }
             } else {
                 btn_ticks_counter = 0;
+            }
+
+            if (btn_click_counter == 1)
+            {
+                PORTE |= (1 << PE3);
+                _delay_ms(500);
+                PORTE &= ~(1 << PE3);
+                btn_click_counter = 0;
             }
         };
     } else {
@@ -78,6 +77,5 @@ int main(void)
             PORTE |= (1 << PE2);
         };
     }
-
     return 0;
 }
